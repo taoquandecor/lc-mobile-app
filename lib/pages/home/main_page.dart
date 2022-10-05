45849f78686dcbc504ce3d9bb497c4e0e2065731
taoquandecor <taoquandecor@gmail.com>
2022-10-05 17:02:14 +0700

    ok

--- lib/pages/home/main_page.dart
+++ lib/pages/home/main_page.dart
@@ -5,6 +5,22 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: const TabBar(tabs: [
+            Tab(icon: Icon(Icons.list)),
+            Tab(icon: Icon(Icons.anchor)),
+          ]),
+        ),
+        body: const TabBarView(
+          children: [
+            Icon(Icons.directions_car),
+            Icon(Icons.directions_transit),
+          ],
+        ),
+      ),
+    );
   }
 }
